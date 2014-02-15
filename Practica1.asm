24c62388b2949b0fb0626e6bc4b00971df4d0d0e
Sebastian Garces Montoya <sgarcesmo@gmail.com>
2014-02-14 21:05:52 -0500

    FUNCIONA ORGANIZAR ENTEROS

--- Practica1.asm
+++ Practica1.asm
@@ -5,9 +5,9 @@ LDB 150     ;carga en AX lo de BX + mem
 INC 105     ; incremento el apuntador de la lista
 CMP 108
 JEQ 00A     ;SALTOOOOOOOOOOOOO
-JMP 02E
+JMP 02F     ;SALTOOOOOOOOOOOOO
 LOOP 001    ;SALTOOOOOOOOOOOOO
-JMP 012     ;SALTOOOOOOOOOOOOO
+JMP 03F     ;SALTOOOOOOOOOOOOO
 HLT
    INC 103 ; incrementa contador de enteros
    MOV BX,105
@@ -16,6 +16,7 @@ HLT
    MOV BX,106
    STB 200
    INC 106
+   INC 105
    JMP 007      ;SALTOOOOOOOOOOOOOOOOOO
 
    MOV 106,108 ; pongo en 0 el puntero a enteros
@@ -25,30 +26,30 @@ HLT
    MOV 101,AX
    INC 106      ;incremento puntero lista enteros
    CMP 10D
-   JMA 029      ;SALTOOOOOOOOO
+   JMA 02A      ;SALTOOOOOOOOO
    CMP 102
-   JMA 024      ;SALTOOOOOOOOO
-   JMP 01F      ;SALTOOOOOOOOO
-   LOOP 014     ;SALTOOOOOOOOO
+   JMA 025      ;SALTOOOOOOOOO
+   JMP 020      ;SALTOOOOOOOOO
+   LOOP 015     ;SALTOOOOOOOOO
    JMP 009      ;SALTOOOOOOOOO
 
    INC 10A      ; incrementa contador casillero 1
    MOV BX,109
    STB 500
    INC 109
-   JMP 01D      ;SALTOOOOOOOOO
+   JMP 01E      ;SALTOOOOOOOOO
 
    INC 10C      ; incrementa contador casillero 2
    MOV BX,10B
    STB 600
    INC 10B
-   JMP 01D      ;SALTOOOOOOOOO
+   JMP 01E      ;SALTOOOOOOOOO
    
    INC 10E      ;incrementa contador casillero 3
    MOV BX,10F
    STB 700
    INC 10F
-   JMP 01D      ;SALTOOOOOOOOO
+   JMP 01E      ;SALTOOOOOOOOO
    
    INC 104 ; incremento flotantes ingresados
    MOV BX,105
@@ -64,10 +65,83 @@ HLT
    MOV BX,107
    STB 300
    INC 107
+   INC 105
    JMP 007      ;SALTOOOOOOOOOOOOOOO
 
+#BUBBLE SORT ENTEROS
+
+LDA 084   ; BUBBLE SORT se carga la variable de reinicio
+STA 081   ; se reinicia el valor de i
+STA 083   ; se reinicia el valor de j
+MOV AX,110 ; Numero de veces del primer ciclo
+STA 080  ; almacena el limite  del ciclo 1
+DEC 110   ; N-1
+MOV AX,110 ; Numero de veces del segundo ciclo
+STA 082  ; almacena el limite del ciclo 2
+LDA 081  ; cargar i
+CMP 080  ; compara i con el limite ciclo 1
+JMA 013  ; condicional, si no cumple salta al final del programa
+    INC 081    ; se incrementa i ( i=i+1)
+    LDA 083    ; cargar j
+    CMP 082    ; compara j con el limite del ciclo 2
+    JMA 062   ; condicional, si no cumple si no cumple salta al jump del ciclo externo
+        INC 083       ; se incrementa j( j=j+1)
+        MOV BX,087
+        LDB 200
+        EAP
+        INC 087
+        STA 085
+        MOV BX,087
+        LDB 200
+        EAP
+        INC 087
+        STA 089
+        MOV BX,087
+        LDB 200
+        DEC 087
+        LDA 085
+        CMP 089
+        JMA 06E
+        INC 091
+        INC 092
+    JMP 04B    ; salta a cargar j
+    LDA 084    ; se carga la variable de reinicio
+    STA 083    ; se reinicia el valor de j para la siguiente iteracion del ciclo de i
+    LDA 088
+    STA 085
+    STA 086
+    STA 087
+    STA 089
+    STA 090
+    STA 091
+    STA 092
+JMP 047   ; salta a cargar i
+HLT       ; fin
+        MOV BX,091
+        LDA 085
+        STB 201
+        MOV BX,092
+        LDA 089
+        STB 200
+        JMP 05F
+
+#080
+0  ; 80 LIMITE CICLO 1
+1  ; 81 I
+0  ; 82 LIMITE CICLO 2
+1  ; 83 J
+1  ; 84 REINICIO DE VARIABLES
+0  ; 85 VARIABLE AUXILIAR
+0  ; 86 ACUMULADOR DE RECORRIDO
+0  ; 87 PUNTERO DE RECORRIDO
+0  ; 88 REINICIO DE APUNTADORES
+0  ; 89 J+1
+0  ; 90 TEMPORAL
+0  ; 91 TEMPORAL
+0  ; 92 TEMPORAL
+
 #100
-10010  ; 100 tamaño
+11010  ; 100 TAMANO!!!!!!!!!!!!!!!
 0    ; 101 VARIABLE AUXILIAR PARA BUBBLE SORT Y COSITAS VARIAS
 1010101; 102 CRITERIO DE SELECCION CASILLERO 2 (85 Decimal)
 0    ; 103 enteros ingresados
@@ -83,7 +157,7 @@ HLT
 10101010 ; 10D CRITERIO SELECCION CASILLERO 3  (170 Decimal)
 0    ; 10E TAMAÑO CASILLERO 3
 0    ; 10F PUNTERO CASILLERO 3   ( 700)
-
+1101 ; 110 Numero de datos ingresados !!!!!!!!!!!!!!!!!!!!!!!!
 
 #150
 0
@@ -100,6 +174,18 @@ HLT
 11010
 0
 11111101
+1
+0100000000000000
+0000000000000000
+1
+0100000011000000
+0000000000000000
+1
+0100000000000000
+0000000000000000
+1
+0100000011000000
+0000000000000000
 1
 0100000000000000
 0000000000000000
